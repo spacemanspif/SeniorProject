6088241f2526628146339322b666a51a51cad5ad
EvanKent <evankent@live.com>
2015-02-02 12:34:36 -0800

    Started work on finding files. Currently just prints

--- SeniorProject/SeniorProject/Program.cs
+++ SeniorProject/SeniorProject/Program.cs
@@ -14,55 +14,74 @@ namespace SeniorProject
     {
         static void Main(string[] args)
         {
-            string fp = "D:/Music";
+            //Filepath, eventually given by user, that will be checked for music
+            string filePath = "D:/Music";
             
-            Boolean hasDir = ContainsDir(fp);
+            //runs method to check for subdirecs
+            Boolean hasDir = ContainsDir(filePath);
             Console.WriteLine(hasDir);
 
-            if(hasDir == true)
+            //if it has a subdirectory, checks those subdirectories
+            if(hasDir)
             {
-                Iterate(fp,0);
+                Iterate(filePath,0);
             }
         }
 
-        static Boolean ContainsDir(string fp)
+        //method to check directories for subdirectories, and returns a true or false
+        static Boolean ContainsDir(string filePath)
         {
-            Boolean hasDir = false;
             try
             {
-                DirectoryInfo dir = new DirectoryInfo(fp);
-                int layers = dir.GetDirectories().Length;
-                if(layers != 0)
-                {
-                    hasDir = true;
-                }
-
+                DirectoryInfo dir = new DirectoryInfo(filePath);
+                //if GetDirectories returns anything other than 0 (no subdirecs), sends out true
+                if (dir.GetDirectories().Length > 0) return true;
             }
             catch(DirectoryNotFoundException dnfe)
             {
                 Console.WriteLine("Directory not found", dnfe.Message);    
             }
-            return hasDir;
+            //otherwise returns false
+            return false;
         }
-
-        static void Iterate(string fp, int count)
+        //method to take a filepath, and int representing which layer of subdir its in, and find, display (and eventually copy) information about all subdirecs in it
+        static void Iterate(string filePath, int layer)
         {
-            DirectoryInfo di = new DirectoryInfo(fp);
-            List<DirectoryInfo> dil = di.GetDirectories().ToList();
-            foreach (DirectoryInfo dilIt in dil)
-            {
-                fp = dilIt.FullName;
-                Console.WriteLine(dilIt.Name + ": " + ContainsDir(fp));
 
-                if(ContainsDir(fp))
+            DirectoryInfo dir = new DirectoryInfo(filePath);
+            List<DirectoryInfo> dirList = dir.GetDirectories().ToList();
+
+            //iterates through each iten in the list
+            foreach (DirectoryInfo currentDir in dirList)
+            {
+                //changes the filepath to the newest directory
+                filePath = currentDir.FullName;
+                //if the directory is a subdir (or subsubdir), writes a tab for each "sub"
+                for (int i = 0; i < layer; i++)
                 {
-                    count++;
-                    for (int i=0; i < count;i++ )
-                    {
-                        Console.Write("   ");
-                    }
-                    Iterate(fp,count);
-                    count--;
+                    Console.Write("    ");
+                }
+                //then writes the name, and if it contains any more subdirs
+                Console.WriteLine(currentDir.Name + ": " + ContainsDir(filePath));
+
+                //TESTING FindSongs
+                FindSongs(filePath, layer);
+
+                //then checks for any other subdirs, to continue on the work
+                if (ContainsDir(filePath)) Iterate(filePath, layer + 1);
+            }
+        }
+        static void FindSongs(string filePath, int layer)
+        {
+            DirectoryInfo dirIn = new DirectoryInfo(filePath);
+            List<FileInfo> fileIn = dirIn.GetFiles().ToList();
+            
+            foreach (FileInfo fileInIt in fileIn)
+            {
+                for (int i = 0; i <= layer; i++)
+                {
+                    Console.Write("    ");
+                    Console.WriteLine(fileInIt.Name);
                 }
             }
         }
